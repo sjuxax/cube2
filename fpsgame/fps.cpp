e91fab2fbf7fef6b3b07a6b381f5dd6c9bd53557
Jeff Cook <jeff@deserettechnology.com>
2010-05-18 06:00:07 -0600

    Reset kill and teamkill counter on new round. ...
    I don't know how this never made into the SVN/GitHub lines.

--- fpsgame/fps.cpp
+++ fpsgame/fps.cpp
@@ -524,6 +524,8 @@ namespace game
             fpsent *d = players[i];
             d->frags = d->flags = 0;
             d->deaths = 0;
+            d->kills = 0;
+            d->teamkills = 0;
             d->totaldamage = 0;
             d->totalshots = 0;
             d->maxhealth = 100;
